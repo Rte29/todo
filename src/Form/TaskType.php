7c626229038993bfaa8706d8907b82ac9e1fad8b
R1 <er.gouez@gmail.com>
2022-12-08 15:25:30 +0100

    mise en forme bootstrap 5.1

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -6,14 +6,16 @@ use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class TaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content');
+            ->add('title', TextType::class, ['label' => 'Titre'])
+            ->add('content', TextAreaType::class, ['label' => 'Contenu']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
